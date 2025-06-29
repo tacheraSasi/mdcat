88966c57dde4be943c67776d97c0ba436b807f41
tacherasasi <tacherasasi@gmail.com>
2025-06-29 22:18:12 +0300

    Update copyright information in features.rs to reflect new author

--- tests/features.rs
+++ tests/features.rs
@@ -1,4 +1,4 @@
-// Copyright 2024 Sebastian Wiesner <sebastian@swsnr.de>
+// Copyright 2025 Tachera Sasi <tachera@ekilie.com>
 
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
